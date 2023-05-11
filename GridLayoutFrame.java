b627b9820f2ac263280b09d74a1a6ec05fc3d299
yaranandes7131 <124922751+yaranandes7131@users.noreply.github.com>
2023-05-11 15:59:48 -0300

    Add files via upload

---
+++ GridLayoutFrame.java
@@ -0,0 +1,45 @@
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class GridLayoutFrame extends JFrame implements ActionListener {
+    private JButton[] buttons;
+    private static final String[] names ={"one","two","three","four","five","six"};
+    private boolean toggle = true;
+    private Container container;
+    private GridLayout gridLayout1;
+    private GridLayout gridLayout2;
+    
+    public GridLayoutFrame()
+    {
+     super("GridLayout Demo");
+     gridLayout1 = new GridLayout(2, 3, 5, 5);
+     gridLayout2 = new GridLayout(3, 2);
+     container = getContentPane();
+     setLayout( gridLayout1 );
+     buttons = new JButton[names.length];
+    
+    for( int count = 0; count < names.length; count++)
+    {
+         buttons[count] = new JButton( names[count]);
+         buttons[count].addActionListener(this);
+         add( buttons[count]);
+    
+    }
+    
+    }
+    
+    @Override
+    public void actionPerformed( ActionEvent event) {
+        if( toggle)
+            container.setLayout(gridLayout2);
+        else
+            container.setLayout(gridLayout1);
+
+        toggle = !toggle;
+        container.validate();
+    }
+}
